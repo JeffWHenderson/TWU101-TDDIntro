61b9bb3c6ce4161d0ab2ee64629aa72efe7e17bb
JeffWHenderson <jeffrey.henderson17@gmail.com>
2018-04-25 18:39:52 -0400

    wrote a test to check that account has a balance. set up constructor to initialize with balance.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,4 +1,9 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
+    public int balance;
+
+    public Account(int initialBalance){
+        this.balance = initialBalance;
+    }
 }
--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,11 +3,35 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+//        List<String> strings = new ArrayList<String>();
+//                StringJoiner joiner = new StringJoiner();
+//                String result = joiner.join(strings);
+//                assertThat(result, is(""));
 public class AccountTests {
+
     @Test
-    @Ignore  // Remove each @Ignore and implement test
+    public void accountIsInitializedWithBalance(){
+        //arrange
+        Account account = new Account(100);
+        //action
+        //assert
+        assertThat(account.balance, is(100));
+    }
+
+    @Test
+    @Ignore
     public void shouldIncreaseMyBalanceWhenIDepositMoney(){
-        Account account = new Account();
+//        //arrange
+//        Account account = new Account();
+//
+//        //action
+//        account.deposit(50);
+//
+//        //assert
+//        assertThat(account.getBalance(), is(150));
     }
 
     @Test
